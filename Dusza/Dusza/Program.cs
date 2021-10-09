62b22e6b9876b4823f31117529246b7792b3d51b
beni69 <73666440+beni69@users.noreply.github.com>
2021-10-09 13:13:08 +0200

    task two done

--- Dusza/Dusza/Program.cs
+++ Dusza/Dusza/Program.cs
@@ -53,7 +53,11 @@ namespace Dusza
 
             // print shit
             PrintMeasures(Measures);
+            Console.WriteLine();
 
+            new TaskTwo { Measures = Measures }.Solve();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Dusza/Dusza/TaskTwo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Dusza {
+    class TaskTwo {
+        public List<Measure> Measures;
+
+        // contains all the relevant speed limits
+        enum SpeedLimits : int {
+            sz = 130, m = 130, b = 100, t = 80
+        }
+
+        public bool ExceededSpeedLimit(Measure _Measure) {
+            // Compare different types with their speed limits
+            switch (_Measure.Type) {
+                case "sz":
+                    return _Measure.Speed > (int)SpeedLimits.sz;
+                case "m":
+                    return _Measure.Speed > (int)SpeedLimits.m;
+                case "b":
+                    return _Measure.Speed > (int)SpeedLimits.b;
+                case "t":
+                    return _Measure.Speed > (int)SpeedLimits.t;
+                default:
+                    return false;  // mk is default
+            }
+        }
+
+        public int ExceedSpeedLimitBy(Measure _Measure) {
+            switch (_Measure.Type) {
+                case "sz":
+                    return _Measure.Speed - (int)SpeedLimits.sz;
+                case "m":
+                    return _Measure.Speed - (int)SpeedLimits.m;
+                case "b":
+                    return _Measure.Speed - (int)SpeedLimits.b;
+                case "t":
+                    return _Measure.Speed - (int)SpeedLimits.t;
+                default:
+                    return 0;
+            }
+        }
+
+        public void Solve() {
+            string sol = "";
+
+            foreach (var Measure in Measures)
+                if (Measure.ID == 'B' && ExceededSpeedLimit(Measure))
+                    sol += $"{Measure.Type} {Measure.Region} {Measure.License} {ExceedSpeedLimitBy(Measure)}" + Environment.NewLine;
+
+            sol = sol.Trim();
+
+            Console.WriteLine(sol);
+            File.WriteAllText("TaskTwo.txt", sol);
+        }
+    }
+}
